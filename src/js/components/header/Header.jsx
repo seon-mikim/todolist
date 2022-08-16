4981f874ba1b3c55324b12c78793768a0f20168d
Seon Mi Kim <tjsalsun@gmail.com>
2022-08-16 21:22:02 +0900

    Update Header.jsx

--- src/js/components/header/Header.jsx
+++ src/js/components/header/Header.jsx
@@ -3,7 +3,7 @@ import"../header/header.css"
 function Header() {
     return( <header className="header-area">
             <span>My To Do List</span>
-            <span>React</span>
+            <span>김선미</span>
             </header>
     )
 }
